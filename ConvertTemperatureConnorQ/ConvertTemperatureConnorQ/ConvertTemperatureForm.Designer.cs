468f7c6fa5e49d2db42d48ec4c7320807821ebbc
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-04-04 12:50:38 -0400

    finished temperature converter

---
+++ ConvertTemperatureConnorQ/ConvertTemperatureConnorQ/ConvertTemperatureForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ConvertTemperatureConnorQ
+{
+    partial class frmConvertTemperature
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(215, 45);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 31);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(146, 121);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(240, 34);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Convert to Fahrenheit";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(33, 233);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(24, 25);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "?";
+            this.lblOutput.Visible = false;
+            this.lblOutput.Click += new System.EventHandler(this.lblOutput_Click);
+            // 
+            // frmConvertTemperature
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 361);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtInput);
+            this.Name = "frmConvertTemperature";
+            this.Text = "Convert Temperature by Connor Quinlan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ ConvertTemperatureConnorQ/ConvertTemperatureConnorQ/ConvertTemperatureForm.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 04-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Temperature Converter
+ * This program converts a temperature in celsius to fahrenheit.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertTemperatureConnorQ
+{
+    public partial class frmConvertTemperature : Form
+    {
+
+        // declare local variables
+        double celsius;
+        double fahrenheit;
+
+        public frmConvertTemperature()
+        {
+            InitializeComponent();
+        }
+
+        public void CalculateTemperature()
+        {
+            // calculate answer
+            fahrenheit = celsius * 9 / 5 + 32;
+            
+            // display output
+            lblOutput.Text = "The temperature in fahrenheit is " + fahrenheit;
+            lblOutput.Visible = true;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // convert input to double
+            celsius = double.Parse(txtInput.Text);
+
+            // call calculate procedure
+            this.CalculateTemperature();
+        }
+
+        private void lblOutput_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
