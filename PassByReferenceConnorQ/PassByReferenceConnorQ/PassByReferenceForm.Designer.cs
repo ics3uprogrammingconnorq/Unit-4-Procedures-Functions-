900f788349067684c0697bec1b73dff6b2196e03
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-04-10 13:10:03 -0400

    pass by reference

---
+++ PassByReferenceConnorQ/PassByReferenceConnorQ/PassByReferenceForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PassByReferenceConnorQ
+{
+    partial class frmPassByReference
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.nudInput = new System.Windows.Forms.NumericUpDown();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(363, 141);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 26);
+            this.txtInput.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // nudInput
+            // 
+            this.nudInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudInput.Location = new System.Drawing.Point(353, 187);
+            this.nudInput.Name = "nudInput";
+            this.nudInput.Size = new System.Drawing.Size(120, 26);
+            this.nudInput.TabIndex = 2;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(88, 141);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(239, 72);
+            this.lblInput.TabIndex = 3;
+            this.lblInput.Text = "Number you want to round:\r\n\r\nNumber of decimals:\r\n";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(363, 244);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 33);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmPassByReference
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 402);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.nudInput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "frmPassByReference";
+            this.Text = "Pass By Reference by Connor Quinlan";
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.NumericUpDown nudInput;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ PassByReferenceConnorQ/PassByReferenceConnorQ/PassByReferenceForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassByReferenceConnorQ
+{
+    public partial class frmPassByReference : Form
+    {
+
+        double round;
+        int decimals;
+
+        public frmPassByReference()
+        {
+            InitializeComponent();
+        }
+
+        public void Round(ref double round, int decimals)
+        {
+            round = (10 ^ decimals) * round;
+            round += 0.5;
+            round = Math.Truncate(round);
+            round = round / (10 ^ decimals);
+            MessageBox.Show("The rounded number is: " + round, "Round Program");
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            round = double.Parse(txtInput.Text);
+            decimals = Convert.ToInt32(nudInput.Value);
+
+            Round(ref round, decimals);
+        }
+    }
+}
