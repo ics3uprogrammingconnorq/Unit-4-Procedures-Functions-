7f564bdcd37022d76d9c3199583ac0b265495b39
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-04-12 12:37:21 -0400

    optional parameters

---
+++ OptionalParametersConnorQ/OptionalParametersConnorQ/OptionalParametersForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace OptionalParametersConnorQ
+{
+    partial class frmOptionalParameters
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtProvince = new System.Windows.Forms.TextBox();
+            this.txtAptNumber = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Location = new System.Drawing.Point(302, 254);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(100, 20);
+            this.txtPostalCode.TabIndex = 0;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(302, 127);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(302, 166);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 2;
+            // 
+            // txtProvince
+            // 
+            this.txtProvince.Location = new System.Drawing.Point(302, 208);
+            this.txtProvince.Name = "txtProvince";
+            this.txtProvince.Size = new System.Drawing.Size(100, 20);
+            this.txtProvince.TabIndex = 3;
+            // 
+            // txtAptNumber
+            // 
+            this.txtAptNumber.Location = new System.Drawing.Point(302, 88);
+            this.txtAptNumber.Name = "txtAptNumber";
+            this.txtAptNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtAptNumber.TabIndex = 4;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(59, 43);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(218, 231);
+            this.lblInput.TabIndex = 5;
+            this.lblInput.Text = "Enter your information here:\r\n\r\nApt. Number (optional):\r\n\r\nStreet Address:\r\n\r\nCit" +
+    "y:\r\n\r\nProvince:\r\n\r\nPostal Code:";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(224, 312);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 6;
+            this.btnEnter.Text = "Submit";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // frmOptionalParameters
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 369);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.txtAptNumber);
+            this.Controls.Add(this.txtProvince);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtPostalCode);
+            this.Name = "frmOptionalParameters";
+            this.Text = "Optional Parameters by Connor Quinlan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPostalCode;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtProvince;
+        private System.Windows.Forms.TextBox txtAptNumber;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
---
+++ OptionalParametersConnorQ/OptionalParametersConnorQ/OptionalParametersForm.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 12-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #30 - Address Program
+ * This program gets information from the user and displays it in a message box.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptionalParametersConnorQ
+{
+    public partial class frmOptionalParameters : Form
+    {
+        public frmOptionalParameters()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            if (txtAddress.Text == "")
+            {
+                MessageBox.Show("You didn't input an adress.", "Invalid");
+            }
+            else
+            {
+                if (txtCity.Text == "")
+                {
+                    MessageBox.Show("You didn't input a city.", "Invalid");
+                }
+                else
+                {
+                    if (txtProvince.Text == "")
+                    {
+                        MessageBox.Show("You didn't input a province.", "Invalid");
+                    }
+                    else
+                    {
+                        if (txtPostalCode.Text == "")
+                        {
+                            MessageBox.Show("You didn't input a postal code.", "Invalid");
+                        }
+                        else
+                        {
+                            if (txtAptNumber.Text == "")
+                            {
+                                PrintInfo(txtAddress.Text, txtCity.Text, txtProvince.Text, txtPostalCode.Text);
+                            }
+                            else
+                            {
+                                PrintInfo(txtAptNumber.Text, txtAddress.Text, txtCity.Text, txtProvince.Text, txtPostalCode.Text);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void PrintInfo(string one, string two, string three, string four)
+        {
+            MessageBox.Show(one + ", " + two + ", " + three + ", " + four, "Info");
+        }
+
+        public void PrintInfo(string one, string two, string three, string four, string five)
+        {
+            MessageBox.Show(one + ", " + two + ", " + three + ", " + four + ", " + five, "Info");
+        }
+    }
+}
