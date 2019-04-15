1aaa7a8ee3796937d8af1d2a95f069468e83955e
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-04-15 13:03:54 -0400

    percentage

---
+++ PercentageProgramConnorQ/PercentageProgramConnorQ/PercentageProgramForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PercentageProgramConnorQ
+{
+    partial class frmPercentageProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(87, 98);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(161, 20);
+            this.lblInput.TabIndex = 0;
+            this.lblInput.Text = "Enter your mark here:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(271, 95);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 26);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(206, 156);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 31);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // frmPercentageProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 332);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInput);
+            this.Name = "frmPercentageProgram";
+            this.Text = "Percentage Program by Connor Quinlan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ PercentageProgramConnorQ/PercentageProgramConnorQ/PercentageProgramForm.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 15-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #31 - Percentage Program
+ * This program calculates a percentage based on a given grade by the user.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PercentageProgramConnorQ
+{
+    public partial class frmPercentageProgram : Form
+    {
+        // declare global variable
+        int percentage;
+
+        public frmPercentageProgram()
+        {
+            InitializeComponent();
+        }
+
+        public int CalculatePercentage(string level)
+        {
+            // switch case for level
+            switch (level)
+            {
+                // convert level to percentage
+                case "4++":
+                    percentage = 98;
+                    break;
+                case "4+":
+                    percentage = 95;
+                    break;
+                case "4":
+                    percentage = 87;
+                    break;
+                case "4-":
+                    percentage = 83;
+                    break;
+                case "3+":
+                    percentage = 78;
+                    break;
+                case "3":
+                    percentage = 75;
+                    break;
+                case "3-":
+                    percentage = 72;
+                    break;
+                case "2+":
+                    percentage = 68;
+                    break;
+                case "2":
+                    percentage = 65;
+                    break;
+                case "2-":
+                    percentage = 62;
+                    break;
+                case "1+":
+                    percentage = 58;
+                    break;
+                case "1":
+                    percentage = 55;
+                    break;
+                case "1-":
+                    percentage = 52;
+                    break;
+                case "0+":
+                    percentage = 45;
+                    break;
+                case "0":
+                    percentage = 40;
+                    break;
+                case "0-":
+                    percentage = 35;
+                    break;
+                case "0--":
+                    percentage = 30;
+                    break;
+                case "0---":
+                    percentage = 20;
+                    break;
+                case "0----":
+                    percentage = 10;
+                    break;
+                default:
+                    percentage = -1;
+                    break;
+            }
+
+            // return percentage to display
+            return percentage;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            string level = txtInput.Text;
+            int mark;
+
+            // set mark to the return value of CalculatePercentage function
+            mark = CalculatePercentage(level);
+
+
+            // display mark to user
+            MessageBox.Show("Your mark is: " + mark + "%", "Grade");
+        }
+    }
+}
