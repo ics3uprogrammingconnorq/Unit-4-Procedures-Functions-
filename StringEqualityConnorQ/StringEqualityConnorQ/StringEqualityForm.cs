f1aa6530261779ac47ab0109efd9ca0d021df412
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-04-24 12:05:37 -0400

    string

---
+++ StringEqualityConnorQ/StringEqualityConnorQ/StringEqualityForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringEqualityConnorQ
+{
+    public partial class frmStringEquality : Form
+    {
+        public frmStringEquality()
+        {
+            InitializeComponent();
+        }
+    }
+}
