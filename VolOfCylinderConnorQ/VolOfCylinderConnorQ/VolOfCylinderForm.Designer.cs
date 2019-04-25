cdd50542c951f4e2862c57ea373fff177b539927
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-04-25 12:36:50 -0400

    volume

---
+++ VolOfCylinderConnorQ/VolOfCylinderConnorQ/VolOfCylinderForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace VolOfCylinderConnorQ
+{
+    partial class frmVolOfCylinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHeight.Location = new System.Drawing.Point(258, 117);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 26);
+            this.txtHeight.TabIndex = 0;
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRadius.Location = new System.Drawing.Point(258, 187);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 26);
+            this.txtRadius.TabIndex = 1;
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.Location = new System.Drawing.Point(23, 117);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(192, 96);
+            this.lblEnter.TabIndex = 2;
+            this.lblEnter.Text = "Enter the height here:\r\n\r\n\r\nEnter the radius here:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(168, 247);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmVolOfCylinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 337);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblEnter);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.txtHeight);
+            this.Name = "frmVolOfCylinder";
+            this.Text = "Volume of a Cylinder by Connor Quinlan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ VolOfCylinderConnorQ/VolOfCylinderConnorQ/VolOfCylinderForm.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 25-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #32 - Volume of a Cylinder
+ * This program calculates the volume of a cylinder.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolOfCylinderConnorQ
+{
+    public partial class frmVolOfCylinder : Form
+    {
+        public frmVolOfCylinder()
+        {
+            InitializeComponent();
+        }
+
+        public double CalculateVolume(double height, double radius)
+        {
+            // declare local variable
+            double value;
+
+            // calculate volume
+            value = Math.PI * Math.Pow(radius, 2) * height;
+
+            return value;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            double height;
+            double radius;
+            double volume;
+
+            // convert values to doubles
+            height = Convert.ToDouble(txtHeight.Text);
+            radius = Convert.ToDouble(txtRadius.Text);
+
+            // call function and return volume
+            volume = CalculateVolume(height, radius);
+
+            // round volume
+            volume = Math.Round(volume, 2);
+
+            MessageBox.Show("The volume of the cylinder is: " + volume + " units cubed", "Volume");
+        }
+    }
+}
